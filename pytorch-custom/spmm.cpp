9f59b46cb0679c451b936c011520e37915f43142
fishming <fishmingyu@github.com>
2021-02-01 18:08:39 +0800

    change

--- pytorch-custom/spmm.cpp
+++ pytorch-custom/spmm.cpp
@@ -6,7 +6,8 @@
 torch::Tensor spmm_cuda(
     torch::Tensor rowptr,
     torch::Tensor colind,
-    torch::Tensor dense
+    torch::Tensor edge_weight_csr,
+    torch::Tensor grad_out
 );
 
 // #define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a cuda tensor")
@@ -16,18 +17,22 @@ torch::Tensor spmm_cuda(
 torch::Tensor csr_spmm(
     torch::Tensor A_rowptr,
     torch::Tensor A_colind,
+    torch::Tensor A_csrVal,
     torch::Tensor B
 ) {
     assert(A_rowptr.device().type()==torch::kCUDA);
     assert(A_colind.device().type()==torch::kCUDA);
+    assert(A_csrVal.device().type()==torch::kCUDA);
     assert(B.device().type()==torch::kCUDA);
     assert(A_rowptr.is_contiguous());
     assert(A_colind.is_contiguous());
+    assert(A_csrVal.is_contiguous());
     assert(B.is_contiguous());
     assert(A_rowptr.dtype()==torch::kInt32);
     assert(A_colind.dtype()==torch::kInt32);
+    assert(A_csrVal.dtype()==torch::kFloat32);
     assert(B.dtype()==torch::kFloat32);
-    return spmm_cuda(A_rowptr, A_colind, B);
+    return spmm_cuda(A_rowptr, A_colind, A_csrVal, B);
 }
 
 PYBIND11_MODULE(spmm, m) {
